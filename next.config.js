ff7504b0ad7b6bc912e3d97577c6885a03514b23
PendikWebTasarimcom <karakar@KARAKAR.local>
2026-05-03 20:18:07 +0300

    Fix Vercel build errors - update next.config.js and add jsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
   compress: true,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname),
-  },
 };
 
 module.exports = nextConfig;
